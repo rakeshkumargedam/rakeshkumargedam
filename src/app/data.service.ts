6897e8034201a0b874234706d6b29f1d7be1949a
ndccroyals <Rakeshkumar.gedam.ctr@sabre.com>
2021-03-30 21:57:49 -0500

    added images

---
+++ src/app/data.service.ts
@@ -0,0 +1,29 @@
+import { retry, catchError, tap } from 'rxjs/operators';
+
+export class dataService {
+public first: string = "";  
+public prev: string = "";  
+public next: string = "";  
+public last: string = "";
+
+parseLinkHeader(header) {
+    if (header.length == 0) {
+      return ;
+    }
+
+    let parts = header.split(',');
+    var links = {};
+    parts.forEach( p => {
+      let section = p.split(';');
+      var url = section[0].replace(/<(.*)>/, '$1').trim();
+      var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+
+    });
+
+    this.first  = links["first"];
+    this.last   = links["last"];
+    this.prev   = links["prev"];
+    this.next   = links["next"]; 
+  }
+}
